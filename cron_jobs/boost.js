f48c455475dcfcc455bfc9354bc5d71314c751f2
NathanNyawaya <nyawayanathan@gmail.com>
2024-01-02 16:41:58 +0300

    update the booster

--- cron_jobs/boost.js
+++ cron_jobs/boost.js
@@ -1,32 +1,46 @@
 import Image from "../models/Image.js";
 import cron from "node-cron";
 
+// const viewsBooster = async (threshold, increment) => {
+//     try {
+//         const imagesToUpdate = await Image.find({ viewCount: { $lt: threshold } });
+
+//         if (imagesToUpdate.length > 0) {
+//             const result = await Image.updateMany({ viewCount: { $lt: threshold } }, { $inc: { viewCount: increment, likeCount: 2 } });
+
+//             console.log(`${result.modifiedCount} images updated.`, result);
+
+//             return result;
+//         } else {
+//             console.log('No images found with viewCount less than the specified threshold.');
+//         }
+//     } catch (error) {
+//         console.error(error);
+
+//     }
+// }
+
 const viewsBooster = async (threshold, increment) => {
     try {
-        const imagesToUpdate = await Image.find({ viewCount: { $lt: threshold } });
+        const result = await Image.updateMany(
+            { viewCount: { $lt: threshold } },
+            { $inc: { viewCount: increment, likeCount: 2 } }
+        );
 
-        if (imagesToUpdate.length > 0) {
-            const result = await Image.updateMany({ viewCount: { $lt: threshold } }, { $inc: { viewCount: increment, likeCount: 2 } });
+        console.log(`${result.modifiedCount} images updated.`, result);
 
-            console.log(`${result.modifiedCount} images updated.`, result);
-
-            return result;
-        } else {
-            console.log('No images found with viewCount less than the specified threshold.');
-        }
+        return result;
     } catch (error) {
         console.error(error);
-
     }
-}
-
+};
 
 export const scheduler = () => {
     // Every 10 minutes
     cron.schedule("*/10 * * * *", async () => {
         try {
             console.log('Running cron job every 10 minutes...');
-            await viewsBooster(10000, 3);
+            await viewsBooster(10000, 11);
             console.log('Cron job completed successfully.');
         } catch (error) {
             console.error('Error in cron job:', error);
@@ -37,7 +51,7 @@ export const scheduler = () => {
     cron.schedule("*/30 * * * *", async () => {
         try {
             console.log('Running cron job every 30 minutes...');
-            await viewsBooster(5000, 6);
+            await viewsBooster(5000, 12);
             console.log('Cron job completed successfully.');
         } catch (error) {
             console.error('Error in cron job:', error);
@@ -48,7 +62,7 @@ export const scheduler = () => {
     cron.schedule("0 * * * *", async () => {
         try {
             console.log('Running cron job every hour...');
-            await viewsBooster(3000, 11);
+            await viewsBooster(3000, 31);
             console.log('Cron job completed successfully.');
         } catch (error) {
             console.error('Error in cron job:', error);
@@ -59,7 +73,7 @@ export const scheduler = () => {
     cron.schedule("0 */4 * * *", async () => {
         try {
             console.log('Running cron job every 4 hours...');
-            await viewsBooster(1000, 14);
+            await viewsBooster(1000, 21);
             console.log('Cron job completed successfully.');
         } catch (error) {
             console.error('Error in cron job:', error);
@@ -70,7 +84,7 @@ export const scheduler = () => {
     cron.schedule("0 0,12 * * *", async () => {
         try {
             console.log('Running cron job...');
-            await viewsBooster(20000, 21);
+            await viewsBooster(20000, 111);
             console.log('Cron job completed successfully.');
         } catch (error) {
             console.error('Error in cron job:', error);
